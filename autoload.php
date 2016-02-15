19870418858cdcee189b83af8009361f523e8d2c
Brian Porter <beporter@users.sourceforge.net>
2016-02-15 12:01:26 -0600

    Strip out now-unnecessary Composer dependencies.

--- autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Shim for loading composer's autload file when available.
- */
-
-$composer = dirname(__FILE__) . '/vendor/autoload.php';
-if ( file_exists($composer) ) {
-    require_once $composer;
-}
